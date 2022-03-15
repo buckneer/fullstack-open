30929e43fb5ab108aa3647f376c905da2874b579
Miftari Simel <42391164+buckneer@users.noreply.github.com>
2022-03-15 16:56:25 -0700

    Add files via upload

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,37 @@
+import Content from "./components/Content"
+import Header from "./components/Header"
+import Total from "./components/Total"
+
+
+function App() {
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+    </div>
+    </div>
+  );
+}
+
+export default App;
